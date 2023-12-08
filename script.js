c5ac5e2a789be2db43804785a954ba1cdcab6c7c
Nikhil Akkilla <148036088+nikhilakkilla@users.noreply.github.com>
2023-12-08 09:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('input').change(function(){
+        var inp = $(this).val()
+        $('ul').append(`<li>${inp}<i class="fa-solid fa-pencil"></i><i class="fa-solid fa-trash"></i></li>`)
+        $(this).val('')
+    })
+    
+    $('ul').on('click','.fa-trash', function(){
+        $(this).parent('li').fadeOut(500)
+    })
+    
+    $('ul').on('click','.fa-pencil', function(){
+        $(this).parent('li').toggleClass('checked')
+    })
+})
